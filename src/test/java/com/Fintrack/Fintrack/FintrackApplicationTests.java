8b5203b22171ae977133ff251d130552f7c383bf
sagar-234 <sagar23401@gmail.com>
2025-09-25 02:21:56 +0530

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/com/Fintrack/Fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fintrack.Fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
