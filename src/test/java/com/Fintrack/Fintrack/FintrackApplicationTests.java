52082343418763c2c68aac60cc73467e3375eb65
sagar-234 <sagar23401@gmail.com>
2025-09-26 03:49:42 +0530

    Save local changes before pulling

--- src/test/java/com/Fintrack/Fintrack/FintrackApplicationTests.java
+++ src/test/java/com/Fintrack/Fintrack/FintrackApplicationTests.java
@@ -3,11 +3,11 @@ package com.Fintrack.Fintrack;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FintrackApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class FintrackApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
