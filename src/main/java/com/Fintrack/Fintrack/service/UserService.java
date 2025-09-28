d3d702ba42a0ca611c1b6dde3f71551d8e23f6c0
sagar-234 <sagar23401@gmail.com>
2025-09-28 19:37:00 +0530

    userServiceImpl and deletion of dto layer

--- src/main/java/com/Fintrack/Fintrack/service/UserService.java
+++ src/main/java/com/Fintrack/Fintrack/service/UserService.java
@@ -1,4 +1,14 @@
 package com.Fintrack.Fintrack.service;
 
+import com.Fintrack.Fintrack.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User createUser(User user);
+    User getUserById(Long id);
+    List<User> getAllUsers();
+    User updateUser(Long id,User user);
+    void deleteUser(Long id);
+
 }
--- src/main/java/com/Fintrack/Fintrack/service/UserServiceImpl.java
+++ src/main/java/com/Fintrack/Fintrack/service/UserServiceImpl.java
@@ -1,4 +1,37 @@
 package com.Fintrack.Fintrack.service;
 
+import com.Fintrack.Fintrack.model.User;
+import com.Fintrack.Fintrack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class UserServiceImpl {
+
+
+@Autowired
+private UserRepository userRepository;
+
+public User createUser(User user){
+    return userRepository.save(user);
+
+}
+
+  public User getUserById(Long id){
+return userRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("User not found" + id));
+  }
+public List<User> getAllUsers(){
+    return  userRepository.findAll();
+}
+
+    public User updateUser(Long id,User user){
+    return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found" + id));
+    }
+
+
+
+
 }
