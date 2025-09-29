e3cf7d283d6c272b63612cdfa4c413d45e2b88af
sagar-234 <sagar23401@gmail.com>
2025-09-29 23:29:06 +0530

    Built Controller class for User and turned off spring security for now

--- src/main/java/com/Fintrack/Fintrack/controller/UserController.java
+++ src/main/java/com/Fintrack/Fintrack/controller/UserController.java
@@ -1,4 +1,46 @@
 package com.Fintrack.Fintrack.controller;
 
+
+import com.Fintrack.Fintrack.model.User;
+import com.Fintrack.Fintrack.service.UserService;
+import com.Fintrack.Fintrack.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserServiceImpl userServiceimpl;
+
+    @GetMapping
+    public List<User> getAllUser(){
+        return userServiceimpl.getAllUsers();
+    }
+
+    @PostMapping
+    public  User createUser(@RequestBody User user){
+        return userServiceimpl.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+   public User getUserById(@PathVariable Long id){
+        return userServiceimpl.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id,@RequestBody User user){
+        return userServiceimpl.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id){
+        userServiceimpl.deleteUser(id);
+        return "User with ID" + id + "  " + "has been deleted";
+    }
+
+
 }
--- src/main/java/com/Fintrack/Fintrack/service/UserServiceImpl.java
+++ src/main/java/com/Fintrack/Fintrack/service/UserServiceImpl.java
@@ -8,18 +8,26 @@ import org.springframework.stereotype.Service;
 import java.util.*;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
 
 
 @Autowired
 private UserRepository userRepository;
+
+
+
+
+
+
+
 
 public User createUser(User user){
     return userRepository.save(user);
 
 }
 
-  public User getUserById(Long id){
+
+public User getUserById(Long id){
 return userRepository.findById(id)
         .orElseThrow(() -> new RuntimeException("User not found" + id));
   }
@@ -27,7 +35,8 @@ public List<User> getAllUsers(){
     return  userRepository.findAll();
 }
 
-    public User updateUser(Long id,User user){
+
+public User updateUser(Long id,User user){
     User existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found" + id));
 
     existingUser.setUsername(user.getUsername());
@@ -38,13 +47,11 @@ public List<User> getAllUsers(){
     return userRepository.save(existingUser);
     }
 
-    public void deleteUser(Long id){
+
+public void deleteUser(Long id){
     if (!userRepository.existsById(id)){
         throw new RuntimeException("User does not exists" + id);
     }
     userRepository.deleteById(id);
     }
-
-
-
 }
